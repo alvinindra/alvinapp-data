59946e463a0d55a97a4d5cc16abbaa3a69bad4d6
Alvin Indra Pratama <30944414+alvinindra@users.noreply.github.com>
2019-12-11 13:25:10 +0700

    Update index.php Change Name of Table

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <title>Daftar Pelamar - Alvinapp Data</title>
-    <meta name="description" content="My first PWA" />
+    <meta name="description" content="My First Web App Azure" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="css/materialize.min.css">
@@ -37,7 +37,7 @@
                     $job = $_POST['job'];
                     $date = date("Y-m-d");
                     // Insert data
-                    $sql_insert = "INSERT INTO Registration (name, email, job, date) 
+                    $sql_insert = "INSERT INTO Applicants (name, email, job, date) 
                                 VALUES (?,?,?,?)";
                     $stmt = $conn->prepare($sql_insert);
                     $stmt->bindValue(1, $name);
@@ -52,7 +52,7 @@
                 echo "<h3>Anda telah terdaftar!</h3>";
             } else if (isset($_POST['load_data'])) {
                 try {
-                    $sql_select = "SELECT * FROM Registration";
+                    $sql_select = "SELECT * FROM Applicants";
                     $stmt = $conn->query($sql_select);
                     $applicantsRow = $stmt->fetchAll(); 
                     if(count($applicantsRow) > 0) {
@@ -62,7 +62,7 @@
                         echo "<th>Email</th>";
                         echo "<th>Pekerjaan</th>";
                         echo "<th>Tanggal</th></tr>";
-                        foreach($registrants as $applicants) {
+                        foreach($applicantsRow as $applicants) {
                             echo "<tr><td>".$applicants['name']."</td>";
                             echo "<td>".$applicants['email']."</td>";
                             echo "<td>".$applicants['job']."</td>";
